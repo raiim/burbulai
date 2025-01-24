bc39820b5487ba12db1e8e088c347d5bd24bb348
Matas Reivytis <r.matas@inbox.lt>
2025-01-25 00:25:11 +0200

    add water physics movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,40 +1,37 @@
 extends CharacterBody2D
 
 
-@export var speed = 300
+@export var max_speed = 300
+@export var acceleration = 20
+@export var deceleration = 100
+@export var damping = .95
 @onready var player_skin: Sprite2D = $Sprite2D
 
-
 func _physics_process(_delta: float) -> void:
+		
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	
-	
-	if Input.is_action_pressed("move_right"):
-		player_skin.scale.x = -1
-	elif Input.is_action_pressed("move_left"):
-		player_skin.scale.x = 1
-
-		
 	var direction_normalized = direction.normalized()
 	
-	velocity = direction_normalized * speed
+	# TODO: rotate to the direction the player is moving to
+	if Input.is_action_pressed("move_right"):
+		player_skin.scale.x = 1
+	elif Input.is_action_pressed("move_left"):
+		player_skin.scale.x = -1
+		
+	if Input.is_action_pressed("move_up"):
+		player_skin.scale.y = 1
+	elif Input.is_action_pressed("move_down"):
+		player_skin.scale.y = -1
+	
+	print(direction)
+	
+	
+	if direction != Vector2.ZERO:
+		print("moving!")
+		velocity += direction_normalized * acceleration
+		velocity = velocity.clamp(Vector2(-1 * max_speed, -1 * max_speed), Vector2(max_speed, max_speed))
+	else:
+		velocity *= damping
+		
 	move_and_slide()
-	
-	
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("move_left", "move_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-	#move_and_slide()
