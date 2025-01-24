5746b92f21647a875c1ac4b8364c4d004aa4990b
Raimundas <raimundas@MacBookPro.lan>
2025-01-24 23:07:59 +0200

    Add: input cancel, restart game

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,5 +7,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	
+	if Input.is_action_pressed("quit_game"):
+		get_tree().quit()
+		
+	if Input.is_action_pressed("restart_game"):
+		get_tree().reload_current_scene()
+		
