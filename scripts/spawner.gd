f4eb762747a89810a0c0328108be22866b8e4abd
Raimundas <raimundas@MacBookPro.lan>
2025-01-25 16:57:13 +0200

    imp: fish follow movements

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -7,14 +7,17 @@ extends Marker2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	spawn_fishes()
+	#spawn_fishes()
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn_fishes():
 	var fish_scene = items.pick_random()
 	var new_fish = fish_scene.instantiate()
 	
+	new_fish.set_position(Vector2(randf_range(-200, 1300), randf_range(-200, 200)))
+	
 	add_child(new_fish)
 	
-	#get_tree().create_timer(randf_range(spawn_interval_min, spawn_interval_max)).timeout.connect(spawn_fishes())
+	get_tree().create_timer(randf_range(spawn_interval_min, spawn_interval_max)).timeout.connect(spawn_fishes)
+	
