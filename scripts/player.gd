cac43e4724c3e12d0a337a6d453535812db46a17
Raimundas <raimundas@MacBookPro.lan>
2025-01-26 07:52:44 +0200

    add death out off screen

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,13 +15,25 @@ var projectile_return_timer = 3.0
 var is_shoot_cooldown = false
 var proj_instance
 
+# Offset values for screen bounds
+var offset_x = 200
+var offset_y = 0  # Allow vertical movement above the screen
+var screen_size: Vector2
+
 var player_health: int = 4
+var is_on_damage_cooldown: bool = false
+var left_screen_timer: float = 2.0
+var count = 0
 
 
 func _ready() -> void:
-	pass	
+	screen_size = get_viewport().get_visible_rect().size
 
 func _physics_process(_delta: float) -> void:
+	if player_is_outside_the_screen() and !is_on_damage_cooldown:
+		set_timer()
+		take_damage(1)
+		
 	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	var direction_normalized = direction.normalized()	
 	var _actual_velocity = sqrt(pow(velocity.x, 2) + pow(velocity.y, 2))
@@ -51,6 +63,7 @@ func _physics_process(_delta: float) -> void:
 	#print("rotation: ", _full_player_rotation_deg)
 		
 	move_and_slide()
+
 	
 # TODO: move logic to 'harpoon' entity
 func shoot():
@@ -68,8 +81,25 @@ func shoot_timer():
 func take_damage(damage:int):
 	player_health -= damage
 	ui.health_score = player_health
-	
+
 	if player_health <= 0:
-		print("Player is dead!")
-		get_tree().paused = true
-		ui.get_node("Panel").visible = true
+		show_death_panel()
+
+func show_death_panel():
+	print("Player is dead!")
+	get_tree().paused = true
+	ui.get_node("Panel").visible = true
+
+func set_timer():
+	is_on_damage_cooldown = true
+	await get_tree().create_timer(left_screen_timer).timeout
+	is_on_damage_cooldown = false
+	
+func player_is_outside_the_screen() -> bool:
+	# Get the screen size
+	var offset: int = 200
+	# Check if the player is outside the screen bounds
+	if position.x < -offset or position.x > screen_size.x + offset \
+	or position.y < -offset or position.y > screen_size.y + offset:
+		return true
+	return false	
